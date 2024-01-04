07559116b0a66a6649735d786461abcc3014d56f
domcharrier <docharri@amd.com>
2024-01-04 22:10:34 +0000

    fix(build_pkg.sh): rm shared objects before build

--- build_pkg.sh
+++ build_pkg.sh
@@ -149,7 +149,8 @@ if [ -z ${NO_BUILD+x} ]; then
   PYTHON _render_update_version.py
   #PYTHON -m build ${PKG} -n
   cd ${PKG}
-  PYTHON setup.py build_ext -j ${NUM_JOBS} bdist_wheel
+  find . -name "*.cpython*.so" -delete
+  PYTHON setup.py clean --all build_ext -j ${NUM_JOBS} bdist_wheel
   cd ..
 fi
 
