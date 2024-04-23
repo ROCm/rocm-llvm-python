53d9ba0f0cf0c21791b71ce61f7b8081737df64c
domcharrier <docharri@amd.com>
2024-04-23 11:06:23 +0000

    feat(ci): make conda dir configurable
    * Conda dir defaults to `~/miniconda3` in the scripts
    * Jenkins uses /opt/rocm so that it persists on a
      saved Jenkins docker image.

--- ci/0-install-rocm.sh
+++ ci/0-install-rocm.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # MIT License
-# 
+#
 # Copyright (c) 2023-2024 Advanced Micro Devices, Inc.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -57,9 +57,10 @@ ROCM_VER_SHORT=$(echo $ROCM_VER | sed "s,\([0-9]\+\.[0-9]\+\)\.0,\1,g")
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/rocm-keyring.gpg] https://repo.radeon.com/rocm/apt/${ROCM_VER_SHORT} focal main" | sudo tee /etc/apt/sources.list.d/rocm.list
 echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/rocm-pin-600
 sudo apt update
- 
+
 # install ROCm
 sudo amdgpu-install -y --usecase=rocm --rocmrelease=${ROCM_VER} --no-dkms
 sudo apt install -y rocm-llvm-dev${ROCM_VER} || true # required for ROCM_VER>=6.1.0
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/rocm/lib
+
--- ci/3a-build-and-test-for-single-cpython-version.sh
+++ ci/3a-build-and-test-for-single-cpython-version.sh
@@ -31,8 +31,10 @@ fi
 set -e
 set -o xtrace
 
-source ~/miniconda3/etc/profile.d/conda.sh
+CONDA_DIR=${CONDA_DIR:~/miniconda3}
+source ${CONDA_DIR}/etc/profile.d/conda.sh
 pyver=38
 conda activate py${pyver}
 ./build_pkg.sh --pre-clean --post-clean --run-tests -j ${NUM_JOBS:-16}
 conda deactivate
+
